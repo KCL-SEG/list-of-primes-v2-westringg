461d1b47e443bb0eb9c37c5aaa66b9d4b2434ea0
SeoHyun Kwon <seohyun.kwon@kcl.ac.uk>
2022-10-03 09:05:39 +0100

    Raise ValueError if the input is less than 1

--- primes.py
+++ primes.py
@@ -1,6 +1,31 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from multiprocessing.sharedctypes import Value
+
+
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes<=0:
+        raise(ValueError)
+    else:
+        list = [2]
+        curNum=3
+
+        while len(list)<number_of_primes:
+            isPrime=True
+            divider=2
+            while divider<curNum:
+                if curNum%divider==0:
+                    isPrime=False
+                divider=divider+1
+            
+            if isPrime:
+                list.append(curNum)
+
+            curNum=curNum+1
+
+        return list
+        
+
+
+
